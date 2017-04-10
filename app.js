0da699d29425b2e7959e0feb847fbd1b2599f4d9
Agata <agata@student.edu>
2017-04-10 19:01:11 +0200

    Timeouts are completely wrong

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ document.addEventListener("DOMContentLoaded", function() {
    var points = 0;
    var startButton = document.getElementById("start");
     startButton.onclick = function turnRed() {
+        
+        var initialTimeout = setTimeout(function() {
+           
+           alert("Game over! You've got "+points+" point(s)!");
+           
+                              }, 3000);
        
        var allSquares = [];
        var square = document.getElementsByClassName("btn");
@@ -23,9 +29,16 @@ document.addEventListener("DOMContentLoaded", function() {
        
        redSquare.onclick = function clickedRed() {
            
+           clearTimeout(function(initialTimeout) {});
+           
            redSquare.style.backgroundColor = "powderblue";
            redSquare.removeAttribute("id");
            points = points + 1;
+           setTimeout(function() {
+           
+           alert("Game over! You've got "+points+" point(s)!");
+           
+           }, 3000);
            console.log(points);
            
            turnRed();
