144287ec0c2e4568a3ef0c97f146280fdfed433d
Agata <agata@student.edu>
2017-04-11 15:30:29 +0200

    Timeout reduction works!

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ document.addEventListener("DOMContentLoaded", function() {
    var points = 0;
    var startButton = document.getElementById("start");
    var initialTimeout;
+   var seconds = 10000;
     startButton.onclick = function turnRed() {
         
-       initialTimeout = setTimeout(function() {alert("Game over! You've got "+points+" point(s)!");}, 3000);
+       initialTimeout = setTimeout(function() {alert("Game over! You've got "+points+" point(s)!");}, seconds);
      
        var allSquares = [];
        var square = document.getElementsByClassName("btn");
@@ -26,12 +27,14 @@ document.addEventListener("DOMContentLoaded", function() {
        
        redSquare.onclick = function clickedRed() {
            
+           var redSquare = document.getElementById("red");
            clearTimeout(initialTimeout);
+           seconds = seconds - 1000;
            
            redSquare.style.backgroundColor = "powderblue";
            redSquare.removeAttribute("id");
            points = points + 1;
-           console.log(points);
+           console.log(seconds);
            
            turnRed();
        };
